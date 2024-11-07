67916f109622e8d0aa9ec10bff4525e2c50f6948
Wayne Stark <vanyese1@gmail.com>
2024-11-07 22:29:59 +0300

    exception mapper refactor v2

--- src/main/java/dev/wayne/encryption/exception/ApiExceptionMapper.java
+++ src/main/java/dev/wayne/encryption/exception/ApiExceptionMapper.java
@@ -1,31 +1,16 @@
 package dev.wayne.encryption.exception;
 
-import jakarta.ws.rs.core.Response;
-import jakarta.ws.rs.ext.ExceptionMapper;
-import jakarta.ws.rs.ext.Provider;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
-/**
- * A custom exception mapper `ApiExceptionMapper` that implements the `ExceptionMapper` interface
- * for mapping instances of `ApiException` to appropriate HTTP responses.
- */
-@Slf4j
-@Provider
-public class ApiExceptionMapper implements ExceptionMapper<ApiException> {
+@ControllerAdvice
+public class ApiExceptionMapper {
 
-    /**
-     * This method is invoked when an instance of `ApiException` is thrown,
-     * and it converts the exception to a Response object.
-     *
-     * @param ex The `ApiException` instance that was thrown
-     * @return A Response object with the appropriate status code and entity
-     */
-    @Override
-    public Response toResponse(ApiException ex) {
-        log.error("Response code: " + ex.getErrorCode(), ex);  // Log the error along with the error code
-        return Response
-                .status(ex.getErrorCode())  // Set the HTTP status code based on the error code in the exception
-                .entity(new ApiException(ex.getErrorCode(), ex.getMessage()).toString())  // Create a response entity with JSON-like representation of the exception
-                .build();  // Build the Response object
+    @ExceptionHandler({ApiException.class})
+    public ResponseEntity<Object> handleAll(ApiException ex, WebRequest request) {
+        return new ResponseEntity<>(ex.toString(), HttpStatusCode.valueOf(ex.getStatusCode()));
     }
 }
