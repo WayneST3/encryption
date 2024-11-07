f6ec8785f3eee8da4d80c06a0911f0b9d475d385
Wayne Stark <vanyese1@gmail.com>
2024-11-07 22:29:03 +0300

    exception mapper refactor

--- src/main/java/dev/wayne/encryption/exception/ApiException.java
+++ src/main/java/dev/wayne/encryption/exception/ApiException.java
@@ -2,37 +2,32 @@ package dev.wayne.encryption.exception;
 
 import lombok.Getter;
 
-import java.io.Serial;
-
 @Getter
 public class ApiException extends RuntimeException {
 
-    @Serial
-    private static final long serialVersionUID = 4829382930029382039L;  // Unique identifier for serialization
-
-    private int errorCode;  // The error code associated with the exception
+    private int statusCode;  // The error code associated with the exception
 
     /**
      * Constructor for creating an instance of `ApiException`.
      *
-     * @param errorCode The error code associated with the exception
-     * @param message   The error message
+     * @param statusCode The error code associated with the exception
+     * @param message    The error message
      */
-    public ApiException(int errorCode, String message) {
+    public ApiException(int statusCode, String message) {
         super(message);  // Call the superclass constructor to set the error message
-        this.errorCode = errorCode;  // Set the error code
+        this.statusCode = statusCode;  // Set the error code
     }
 
     /**
      * Constructor for creating an instance of `ApiException` with a cause.
      *
-     * @param errorCode The error code associated with the exception
-     * @param message   The error message
-     * @param cause     The underlying cause of the exception
+     * @param statusCode The error code associated with the exception
+     * @param message    The error message
+     * @param cause      The underlying cause of the exception
      */
-    public ApiException(int errorCode, String message, Throwable cause) {
+    public ApiException(int statusCode, String message, Throwable cause) {
         super(message, cause);  // Call the superclass constructor with an additional cause parameter
-        this.errorCode = errorCode;  // Set the error code
+        this.statusCode = statusCode;  // Set the error code
     }
 
     /**
@@ -42,6 +37,6 @@ public class ApiException extends RuntimeException {
      */
     @Override
     public String toString() {
-        return "{\"errorCode\": " + errorCode + ", \"message\": \"" + getMessage() + "\" }";
+        return "{\n  \"statusCode\": " + statusCode + ",\n  \"message\": \"" + getMessage() + "\"\n}";
     }
 }
